767e80855120a3f8b8d6ec1fcd9814f5a3549b0f
ELMudyr <coco10012002@gmail.com>
2024-10-28 15:11:35 +0100

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["**/*.{html.js}"],
+  content: ["./src/main.js",
+    './index.html'],
   theme: {
     extend: {},
+    colors: {
+      text: '#040212',
+      background: '#f7f7ff',
+      primary: '#2919eb',
+      secondary: '#dddcff',
+      accent: '#ec5cce',
+    }
   },
   plugins: [],
 }
