eae02ecf8a19cba3a80f31d05712e66b5eb60892
ELMudyr <coco10012002@gmail.com>
2024-10-29 23:12:01 +0100

    Implement llama3-8b API

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ document.getElementById('onOff').addEventListener('click', () => {
 
         if (htmlContent) {
           const extractedText = htmlContent.replace(/<[^>]+>/g, '').trim().replace(/\s+/g, ' ');
-          console.log(extractedText);
+          // console.log(extractedText);
           return extractedText;
         } else {
           console.log('Profile section not found.');
@@ -20,26 +20,29 @@ document.getElementById('onOff').addEventListener('click', () => {
 });
 
 
-// const groq = new Groq();
-// async function main() {
-//   const chatCompletion = await groq.chat.completions.create({
-//     "messages": [
-//       {
-//         "role": "system",
-//         "content": "Extract the following information from the HTML text:\n  - Full Name\n  - Location\n  - Job Title\n-Education"
-//       },
-//     ],
-//     "model": "llama3-8b-8192",
-//     "temperature": 1,
-//     "max_tokens": 1024,
-//     "top_p": 1,
-//     "stream": true,
-//     "stop": null
-//   });
-//
-//   for await (const chunk of chatCompletion) {
-//     process.stdout.write(chunk.choices[0]?.delta?.content || '');
-//   }
-// }
-//
-// main();
+import Groq from 'groq-sdk';
+
+const groq = new Groq();
+async function main() {
+  const chatCompletion = await groq.chat.completions.create({
+    "messages": [
+      {
+        "role": "user",
+        "content": `Extract the following : Fullname, Location, Job Title, Work Experience, Education, Technical Skill, Soft Skills. Keep it short, compact to only one lines or few words in JSON. from this text:${extractedText} `
+      }
+    ],
+    "model": "llama3-8b-8192",
+    "temperature": 1.14,
+    "max_tokens": 1024,
+    "top_p": 1,
+    "stream": false,
+    "response_format": {
+      "type": "json_object"
+    },
+    "stop": null
+  });
+
+  console.log(chatCompletion.choices[0].message.content);
+}
+
+main();
