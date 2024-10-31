ac1d47a84360a9a400433a13f7cf6a34c460288f
ELMudyr <coco10012002@gmail.com>
2024-10-31 18:43:19 +0100

    Visual and Functionality Improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,15 @@ export default {
   content: ["./src/popup.js",
     './index.html'],
   theme: {
-    extend: {},
-    colors: {
-      text: '#040212',
-      background: '#f7f7ff',
-      primary: '#2919eb',
-      secondary: '#dddcff',
-      accent: '#ec5cce',
-    }
+    extend: {
+      colors: {
+        text: '#040212',
+        background: '#f7f7ff',
+        primary: '#4608AD',
+        secondary: '#dddcff',
+        accent: '#ec5cce',
+      }
+    },
   },
   plugins: [],
 }
